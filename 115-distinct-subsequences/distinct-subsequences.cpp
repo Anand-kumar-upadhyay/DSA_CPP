4616382c8344e7154595891bfe2e07a10054b7ed
Anand-kumar-upadhyay <anandupadhyay042@gmail.com>
2026-09-06 16:21:00 +0530

    Time: 31 ms (53.36%) | Memory: 27.1 MB (52.57%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -5,13 +5,12 @@ vector<vector<int>>dp;
 int func(int i,int j,string &s, string &t)
 {
     
-    if(j==t.size())
-    {
-    return 1;
-    }
+    if(j==t.size()) return 1;
+
+    if(i>=s.size()) return 0;
+    
     if(dp[i][j]!=-1)return dp[i][j];
 
-    if(i>=s.size())return 0;
 
     int take=func(i+1,j,s,t);
     int leave=0;
